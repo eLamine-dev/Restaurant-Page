852c10cc5c121baa128ee0b38618f6aec898019f
e-lamine <lamine@e-lamine.dev>
2023-04-12 11:53:54 +0100

    changed background exposure

--- src/js/contact.js
+++ src/js/contact.js
@@ -10,10 +10,6 @@ function renderContactPage() {
       'I know these cookies look delicious, but unfortunately this website is just a project to practice ES6 Modules and webpack module bundler from The Odin project Javascript curriculum.';
    contactContainer.appendChild(paragraph1);
 
-   // const paragraph2 = document.createElement('p');
-   // paragraph2.textContent = `Repo `;
-   // contactContainer.appendChild(paragraph2);
-
    const githubLink = document.createElement('a');
    githubLink.href = 'https://github.com/eLamine-dev/Restaurant-Page';
 
